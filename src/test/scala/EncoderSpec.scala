9dc2a3d627b9b2aa0177b9cdf2d7456a2fd4ded6
Cédric Chantepie <cchantep@users.noreply.github.com>
2021-08-02 23:06:59 +0200

    More tests

--- src/test/scala/EncoderSpec.scala
+++ src/test/scala/EncoderSpec.scala
@@ -58,56 +58,90 @@ final class EncoderSpec
     }
 
     "be read as ClassClass2" in {
-      val df = Seq("""{"name":"Foo"}""").toDF
-        .withColumn(
-          "foo",
-          F.from_json(
-            F.column("value"),
-            StructType(
-              Seq(
-                StructField("name", StringType, false),
-                StructField("value", StringType, true)
+      val df =
+        Seq("""{"name":"Foo"}""", """{"name":"Bar","opt":"ipsum"}""").toDF
+          .withColumn(
+            "foo",
+            F.from_json(
+              F.column("value"),
+              StructType(
+                Seq(
+                  StructField("name", StringType, false),
+                  StructField("opt", StringType, true)
+                )
               )
             )
           )
-        )
-        .select("foo.*")
+          .select("foo.*")
 
-      implicit val nameFieldEncoder: TypedEncoder[Name2] = Name2.fieldEncoder
+      implicit def fieldEncoder: TypedEncoder[Name2] = Name2.fieldEncoder
 
-      implicit def optEncoder: TypedEncoder[Option[Name2]] =
-        new TypedEncoder[Option[Name2]] {
-          import org.apache.spark.sql.catalyst.expressions._, objects._
-
-          val nullable: Boolean = true
-
-          val jvmRepr: DataType = ObjectType(classOf[Name2])
-
-          val catalystRepr: DataType = StringType
-
-          def toCatalyst(path: Expression): Expression =
-            nameFieldEncoder.toCatalyst(UnwrapOption(catalystRepr, path))
-
-          def fromCatalyst(path: Expression): Expression = {
-            val javaValue = nameFieldEncoder.fromCatalyst(path)
-
-            val value = NewInstance(
-              classOf[Name2],
-              Seq(javaValue),
-              jvmRepr
-            )
-
-            WrapOption(value, jvmRepr)
-          }
-        }
+      import Name2.optEncoder
 
       implicit val encoder: TypedEncoder[CaseClass2] =
         TypedEncoder.usingDerivation
 
       TypedDataset.createUnsafe[CaseClass2](df).collect().run() must_=== Seq(
-        CaseClass2(new Name2("Foo"), None)
+        CaseClass2(new Name2("Foo"), None),
+        CaseClass2(new Name2("Bar"), Some(new Name2("ipsum")))
       )
     }
+
+    "be read as ClassClass3" in {
+      val df =
+        Seq(
+          """{"id":"XYZ"}""",
+          """{"id":"ABC","lorem":{"name":"Bar"}}""",
+          """{"id":"EFG","lorem":{"name":"ipsum","opt":"dolor"}}"""
+        ).toDF
+          .withColumn(
+            "foo",
+            F.from_json(
+              F.column("value"),
+              StructType(
+                Seq(
+                  StructField("id", StringType, false),
+                  StructField(
+                    "lorem",
+                    StructType(
+                      Seq(
+                        StructField("name", StringType, false),
+                        StructField("opt", StringType, true)
+                      )
+                    ),
+                    true
+                  )
+                )
+              )
+            )
+          )
+          .select("foo.*")
+
+      implicit def idEncoder: TypedEncoder[Id1] = Id1.fieldEncoder
+      implicit def nameEncoder: TypedEncoder[Name2] = Name2.fieldEncoder
+
+      import Name2.optEncoder
+
+      implicit val encoder: TypedEncoder[CaseClass3] =
+        TypedEncoder.usingDerivation
+
+      TypedDataset.createUnsafe[CaseClass3](df).collect().run() must_=== Seq(
+        CaseClass3(new Id1("XYZ"), None),
+        CaseClass3(
+          new Id1("ABC"),
+          Some(CaseClass2(name = new Name2("Bar"), opt = None))
+        ),
+        CaseClass3(
+          new Id1("EFG"),
+          Some(
+            CaseClass2(
+              name = new Name2("ipsum"),
+              opt = Some(new Name2("dolor"))
+            )
+          )
+        )
+      )
+    } tag "wip"
   }
 }
 
@@ -139,6 +173,56 @@ object Name2 {
     def toCatalyst(path: Expression): Expression =
       Invoke(path, "value", jvmRepr)
   }
+
+  implicit def optEncoder: TypedEncoder[Option[Name2]] =
+    new TypedEncoder[Option[Name2]] {
+      import org.apache.spark.sql.catalyst.expressions._, objects._
+
+      val nullable: Boolean = true
+
+      val jvmRepr: DataType = ObjectType(classOf[Name2])
+
+      val catalystRepr: DataType = StringType
+
+      def toCatalyst(path: Expression): Expression =
+        fieldEncoder.toCatalyst(UnwrapOption(catalystRepr, path))
+
+      def fromCatalyst(path: Expression): Expression = {
+        val javaValue = fieldEncoder.fromCatalyst(path)
+
+        val value = NewInstance(
+          classOf[Name2],
+          Seq(javaValue),
+          jvmRepr
+        )
+
+        WrapOption(value, jvmRepr)
+      }
+    }
 }
 
 case class CaseClass2(name: Name2, opt: Option[Name2])
+
+final class Id1(val value: String) extends AnyVal
+
+object Id1 {
+  import org.apache.spark.sql.types.{ DataType, StringType }
+  import org.apache.spark.sql.catalyst.expressions._, objects._
+
+  // Only when Value class is used as struct field
+  def fieldEncoder: TypedEncoder[Id1] = new TypedEncoder[Id1] {
+    val nullable: Boolean = true
+
+    val jvmRepr: DataType = StringType
+
+    val catalystRepr: DataType = StringType
+
+    def fromCatalyst(path: Expression): Expression =
+      TypedEncoder.stringEncoder.fromCatalyst(path)
+
+    def toCatalyst(path: Expression): Expression =
+      Invoke(path, "value", jvmRepr)
+  }
+}
+
+case class CaseClass3(id: Id1, lorem: Option[CaseClass2])
